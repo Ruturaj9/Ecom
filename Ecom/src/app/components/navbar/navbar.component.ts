53398803cc9488fe52518393df14de33ea4ced2f
Ruturaj9 <ruturajmanedeshmukh99@gmail.com>
2025-12-01 19:29:20 +0530

    feat(navbar): implement category dropdown menu with improved navigation

--- Ecom/src/app/components/navbar/navbar.component.ts
+++ Ecom/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { CartService } from '../../services/cart.service';
-
 
 @Component({
   selector: 'app-navbar',
@@ -13,7 +12,10 @@ import { CartService } from '../../services/cart.service';
 export class NavbarComponent {
   mobileOpen = false;
 
-  constructor(public cart: CartService) {}
+  // Dropdown categories
+  categories = ['Vegetables', 'Fruits', 'Grains', 'Dairy', 'Pulses', 'Seeds'];
+
+  constructor(public cart: CartService, private router: Router) {}
 
   toggleMobile() {
     this.mobileOpen = !this.mobileOpen;
@@ -22,5 +24,10 @@ export class NavbarComponent {
   get cartCount() {
     return this.cart.getCart().reduce((sum, item) => sum + item.quantity, 0);
   }
-}
 
+  // Navigate to products page with category filter
+  goToCategory(category: string) {
+    this.mobileOpen = false;
+    this.router.navigate(['/products'], { queryParams: { category } });
+  }
+}
